f9a34e3207268020a3b318991e595088714f50e6
aam2016 <alexander.ananiev@gmail.com>
2017-01-17 17:09:17 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sudo unlink /etc/nginx/sites-enabled/default
-#sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
 
