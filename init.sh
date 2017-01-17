3d6871c482de04ca9a5daf46f1e34a717409a7d0
aam2016 <alexander.ananiev@gmail.com>
2017-01-17 16:02:01 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 #sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
