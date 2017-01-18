c1d227ff705080fad823dba52eb1c0621c7f5eb0
aam2016 <alexander.ananiev@gmail.com>
2017-01-18 14:34:29 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+#1
 #sudo unlink /etc/nginx/sites-enabled/default
 #sudo rm /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-
-#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+#2
+#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#﻿sudo /etc/init.d/mysql start﻿
+
+#3
+#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+#sudo /etc/init.d/nginx restart
