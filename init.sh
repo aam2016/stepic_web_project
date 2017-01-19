772c50f923e677a1460364db0976dcd12c312a7d
aam2016 <alexander.ananiev@gmail.com>
2017-01-19 17:01:03 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -4,8 +4,7 @@
 #sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-# старое sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/gunicorn restart
 ﻿
