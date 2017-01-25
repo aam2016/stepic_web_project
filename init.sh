3f03eb1c351744f4f78cf8effd1a9752b48ec58a
aam2016 <alexander.ananiev@gmail.com>
2017-01-25 17:10:23 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-#sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn.ask   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 #gunicorn -b 0.0.0.0:8080 hello:app
+
+
+#рабочий init.sh
+#sudo rm /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+#sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+#sudo /etc/init.d/gunicorn restart
