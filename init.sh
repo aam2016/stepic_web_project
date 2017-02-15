a073d74e23e101cadff8eddd96b63d8b5d7cb330
aam2016 <alexander.ananiev@gmail.com>
2017-02-15 14:43:31 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
 #source web/myvenv/bin/activate
 #gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
 #gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
