c9696f841087d1ab07e8b08a712c62c5573d9e8d
aam2016 <alexander.ananiev@gmail.com>
2017-02-17 14:28:44 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from os.path import join
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATE_DIRS = (
-    join(BASE_DIR,  'templates'),
-)
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,6 +49,22 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'ask.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'ask.wsgi.application'
 
