0605269f090f5261414b8e2774f8a3d3b6e0d72a
aam2016 <alexander.ananiev@gmail.com>
2017-02-22 12:31:26 +0300

    Signed-off-by: aam2016 <alexander.ananiev@gmail.com>

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -10,8 +10,9 @@ class AskForm(forms.Form):
     title = forms.CharField(max_length=100)
     text = forms.CharField(widget=forms.Textarea)
 
-    def clean(self):
-        pass
+    def clean_text(self):
+        text = self.cleaned.data['text']
+        return text
 
     def save(self):
         question = Question(**self.cleaned_data)
@@ -32,8 +33,9 @@ class AnswerForm(forms.Form):
             question = None
         return question
 
-    def clean(self):
-        pass
+    def clean_text(self):
+        text = self.cleaned.data['text']
+        return text
 
     def save(self):
         answer = Answer(**self.cleaned_data)
